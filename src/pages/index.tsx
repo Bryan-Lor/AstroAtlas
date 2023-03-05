d3a5b5010645fbb45dd4da7a5b8178dbda2d05ea
Kidd <kiddochang02@gmail.com>
2023-03-05 17:39:32 -0500

    Add responsiveness for 1536px+

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,18 +11,18 @@ const Home: NextPage = () => {
   return (
     <>
       <HtmlHead />
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="font-megrim text-8xl font-normal tracking-widest text-white">
+      <div className="flex min-h-full flex-1 flex-col items-center justify-evenly">
+        <h1 className="font-megrim text-8xl font-normal tracking-widest text-white 2xl:text-9xl">
           <span className="text-red-600">Astro</span>Atlas
         </h1>
-        <div className="w-[30rem]">
+        <div className="w-[30rem] 2xl:w-[40rem]">
           <img
             className=""
             src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c4fc1ac7-aaf6-4056-94f6-d96eef44e7c3/dbx8ae7-3da21a85-2fb1-498d-926a-43eaf28d7c24.png/v1/fill/w_1024,h_1024,strp/astronaut_design_by_artbox99_dbx8ae7-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2M0ZmMxYWM3LWFhZjYtNDA1Ni05NGY2LWQ5NmVlZjQ0ZTdjM1wvZGJ4OGFlNy0zZGEyMWE4NS0yZmIxLTQ5OGQtOTI2YS00M2VhZjI4ZDdjMjQucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.l-DoBgY6CmQCU2v4e4nTPPHCLbbmvg2LeZgX0XhgPvE"
           />
         </div>
         <Link
-          className="rounded-full bg-white/20 py-3 px-20 font-mono text-xl tracking-wide text-white"
+          className="rounded-full bg-black/75 py-3 px-20 font-mono text-xl tracking-wide text-white 2xl:px-40"
           href="./app"
         >
           Explore
